37f84645ff382b983bd2854d9311b225be68b96e
pushpeshthakur <pushpeshthakur1626@gmail.com>
2026-01-19 12:36:15 +0530

    Add files via upload
    done

---
+++ script.js
@@ -0,0 +1,75 @@
+const balance = document.getElementById('balance');
+const income = document.getElementById('income');
+const expense = document.getElementById('expense');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+
+function addTransaction(e) {
+  e.preventDefault();
+
+  const transaction = {
+    id: Date.now(),
+    text: text.value,
+    amount: +amount.value
+  };
+
+  transactions.push(transaction);
+  updateLocalStorage();
+  init();
+
+  text.value = '';
+  amount.value = '';
+}
+
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  const item = document.createElement('li');
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text}
+    <span>${sign}₹${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+function updateValues() {
+  const amounts = transactions.map(t => t.amount);
+  const total = amounts.reduce((acc, item) => acc + item, 0).toFixed(2);
+  const incomeTotal = amounts.filter(item => item > 0)
+    .reduce((acc, item) => acc + item, 0).toFixed(2);
+  const expenseTotal = (
+    amounts.filter(item => item < 0)
+    .reduce((acc, item) => acc + item, 0) * -1
+  ).toFixed(2);
+
+  balance.innerText = `₹${total}`;
+  income.innerText = `₹${incomeTotal}`;
+  expense.innerText = `₹${expenseTotal}`;
+}
+
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+form.addEventListener('submit', addTransaction);
